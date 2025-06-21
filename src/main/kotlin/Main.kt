6c228cf3b0554b9ac3a18e5400e200af4dd002cc
João Victor Cardoso <joaovictorcardoso0007@gmail.com>
2025-06-20 23:20:54 -0300

    👻 Event

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 package com.dukita
 
+// Events
+import com.dukita.events.Mention
+
+// Config
 import io.github.cdimascio.dotenv.dotenv
+
+// JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
@@ -13,11 +19,13 @@ fun main() {
 
     val token = dotenv["DISCORD_TOKEN"] ?: error("There was an error finding the token 🛑")
 
-    val jda = JDABuilder.createLight(token, emptyList())
+    val jda = JDABuilder.createDefault(token)
         .setActivity(
             Activity.customStatus("<3 Just a test!")
         )
+
         .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+        .addEventListeners(Mention())
         .build()
 
     jda.awaitReady()
