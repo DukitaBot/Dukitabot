afd5b9e603881b2d439be33324ebe196141c1be6
João Victor Cardoso <joaovictorcardoso0007@gmail.com>
2025-06-20 23:22:36 -0300

    👻 Event

---
+++ src/main/kotlin/events/Mention.kt
@@ -0,0 +1,27 @@
+package com.dukita.events
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+class Mention : ListenerAdapter() {
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        if (event.author.isBot || event.isWebhookMessage) return
+
+
+        val selfUser = event.jda.selfUser
+        val message = event.message
+
+        val messageContent = message.contentRaw
+
+        val mention = selfUser.asMention
+        val nicknameMention = "<@!${selfUser.id}>"
+
+        val startsWithMention = messageContent.startsWith(mention) || messageContent.startsWith(nicknameMention)
+
+        if (startsWithMention) {
+            val authorMention = event.author.asMention
+
+            message.reply("Hii ${authorMention}! <3").queue()
+        }
+    }
+}
